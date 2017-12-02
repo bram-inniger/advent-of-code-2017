ba7f8822685950024d36b744d5cae48e318e6980
Bram Inniger <braminniger@gmail.com>
2017-12-02 18:25:10 +0200

    Updated README.md, also explain the project in the Main method instead of running one arbitrary test

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Advent of Code
 Repository to store solutions for <https://adventofcode.com>
 
-# TODO
-Expand on this README.md
+# About
+
+Every problem is described in under ["Problems"](src/main/resources/problems).  
+Every input is given in ["Inputs"](src/main/resources/inputs).  
+The actual problem solutions (both parts) can be found under ["Main"](src/main/java/be/inniger/advent).  
+Tests verifying all solutions (and sample inputs) can be found under ["Test"](src/test/java/be/inniger/advent).  
--- src/main/java/be/inniger/advent/Main.java
+++ src/main/java/be/inniger/advent/Main.java
@@ -1,17 +1,18 @@
 package be.inniger.advent;
 
-import static be.inniger.advent.util.InputFileReader.read;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import be.inniger.advent.days01to10.Day02;
 
 public class Main {
 
   private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
   public static void main(String... args) {
-    LOG.info("{}", new Day02().solveFirst(read("02.txt")));
+    LOG.info("ABOUT:");
+    LOG.info("");
+    LOG.info("Every problem is described in \"src/main/resources/problems\".");
+    LOG.info("Every input is given in \"src/main/resources/inputs\".");
+    LOG.info("The actual problem solutions (both parts) can be found under \"src/main/java/be/inniger/advent/daysXXtoYY\".");
+    LOG.info("Tests verifying all solutions (and sample inputs) can be found under \"src/test/java/be/inniger/advent/daysXXtoYY\".");
   }
 }
