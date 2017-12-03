e977e4c636b5f621913d6d9f17ee563280a0bed5
Bram Inniger <braminniger@gmail.com>
2017-12-03 20:48:45 +0200

    Further simplified Day 1

--- src/main/java/be/inniger/advent/solutions/Day01.java
+++ src/main/java/be/inniger/advent/solutions/Day01.java
@@ -1,6 +1,5 @@
 package be.inniger.advent.solutions;
 
-import java.util.function.BiPredicate;
 import java.util.stream.IntStream;
 
 import be.inniger.advent.DailyProblem;
@@ -9,19 +8,17 @@ public class Day01 implements DailyProblem<String> {
 
   @Override
   public int solveFirst(String input) {
-    return solve(input,
-        (str, i) -> str.charAt(i) == str.charAt((i + 1) % str.length()));
+    return solve(input, 1);
   }
 
   @Override
   public int solveSecond(String input) {
-    return solve(input,
-        (str, i) -> str.charAt(i) == str.charAt((i + str.length() / 2) % str.length()));
+    return solve(input, input.length() / 2);
   }
 
-  private int solve(String input, BiPredicate<String, Integer> predicate) {
+  private int solve(String input, int offset) {
     return IntStream.range(0, input.length())
-        .filter(i -> predicate.test(input, i))
+        .filter(i -> input.charAt(i) == input.charAt((i + offset) % input.length()))
         .map(input::charAt)
         .map(Character::getNumericValue)
         .sum();
