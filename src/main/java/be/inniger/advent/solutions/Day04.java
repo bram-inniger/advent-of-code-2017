6076572fd1e26a311c65a7a6fed0347b63fd7241
Bram Inniger <braminniger@gmail.com>
2017-12-05 13:46:01 +0200

    Solved Day 4 - Part 2

--- src/main/java/be/inniger/advent/solutions/Day04.java
+++ src/main/java/be/inniger/advent/solutions/Day04.java
@@ -3,8 +3,10 @@ package be.inniger.advent.solutions;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 import be.inniger.advent.DailyProblem;
@@ -12,20 +14,32 @@ import be.inniger.advent.DailyProblem;
 public class Day04 implements DailyProblem<List<String>> {
 
   @Override
-  public int solveFirst(List<String> input) {
-    return (int) input.stream()
-        .filter(this::isValid)
-        .count();
+  public int solveFirst(List<String> inputs) {
+    return solve(inputs, identity());
   }
 
   @Override
-  public int solveSecond(List<String> input) {
-    throw new UnsupportedOperationException();
+  public int solveSecond(List<String> inputs) {
+    return solve(inputs, this::sortLetters);
   }
 
-  private boolean isValid(String passphrase) {
+  private int solve(List<String> passphrases, Function<String, String> mapper) {
+    return (int) passphrases.stream()
+        .filter(passphrase -> isValid(passphrase, mapper))
+        .count();
+  }
+
+  private String sortLetters(String word) {
+    return Pattern.compile("")
+        .splitAsStream(word)
+        .sorted()
+        .collect(joining());
+  }
+
+  private boolean isValid(String passphrase, Function<String, String> mapper) {
     return Pattern.compile("\\s+")
         .splitAsStream(passphrase)
+        .map(mapper)
         .collect(groupingBy(
             identity(),
             counting()))
--- src/test/java/be/inniger/advent/solutions/Day04Test.java
+++ src/test/java/be/inniger/advent/solutions/Day04Test.java
@@ -28,4 +28,28 @@ public class Day04Test {
     assertThat(problem.solveFirst(read(FILE_NAME)))
         .isEqualTo(383);
   }
+
+  @Test
+  public void validateSecondSampleInputs() {
+    assertThat(problem.solveSecond(singletonList("abcde fghij")))
+        .isEqualTo(1);
+
+    assertThat(problem.solveSecond(singletonList("abcde xyz ecdab")))
+        .isEqualTo(0);
+
+    assertThat(problem.solveSecond(singletonList("a ab abc abd abf abj")))
+        .isEqualTo(1);
+
+    assertThat(problem.solveSecond(singletonList("iiii oiii ooii oooi oooo")))
+        .isEqualTo(1);
+
+    assertThat(problem.solveSecond(singletonList("oiii ioii iioi iiio")))
+        .isEqualTo(0);
+  }
+
+  @Test
+  public void validateSecondSolution() {
+    assertThat(problem.solveSecond(read(FILE_NAME)))
+        .isEqualTo(265);
+  }
 }
