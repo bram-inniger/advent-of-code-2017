6b01f2b0c436f46c9a8975e0160605e81e57fdaf
Bram Inniger <braminniger@gmail.com>
2017-12-05 23:39:05 +0200

    Solved Day 3 - Part 2

--- src/main/java/be/inniger/advent/solutions/Day03.java
+++ src/main/java/be/inniger/advent/solutions/Day03.java
@@ -1,13 +1,33 @@
 package be.inniger.advent.solutions;
 
+import static be.inniger.advent.solutions.Day03.Point.point;
 import static java.lang.Math.abs;
 import static java.lang.Math.floor;
 import static java.lang.Math.sqrt;
+import static java.util.Arrays.asList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import be.inniger.advent.DailyProblem;
 
 @SuppressWarnings("PointlessArithmeticExpression")
 public class Day03 implements DailyProblem<Integer> {
+
+  private final List<Point> neighbours = asList(
+      point(1, 0),
+      point(1, 1),
+      point(0, 1),
+      point(-1, 1),
+      point(-1, 0),
+      point(-1, -1),
+      point(0, -1),
+      point(1, -1));
 
   /*
    * The easiest to spot recurring theme on the spiral is that the bottom-right diagonal contains all the odd-numbered squares.
@@ -20,37 +40,73 @@ public class Day03 implements DailyProblem<Integer> {
    */
   @Override
   public int solveFirst(Integer input) {
+    Point coordinates = getCoordinates(input);
+    return abs(coordinates.x) + abs(coordinates.y);
+  }
+
+  /*
+   * Simply iterate over all spiral-points, calculating and storing the new value, until the original input is surpassed.
+   */
+  @Override
+  public int solveSecond(Integer input) {
+    Map<Point, Integer> grid = new HashMap<>();
+    grid.put(point(0, 0), 1);
+
+    int position = 2;
+    int value = 0;
+
+    while (value <= input) {
+      Point coordinate = getCoordinates(position++);
+      value = getNeighboursSum(coordinate, grid);
+      grid.put(coordinate, value);
+    }
+
+    return value;
+  }
+
+  private int getNeighboursSum(Point coordinate, Map<Point, Integer> grid) {
+    return neighbours.stream()
+        .map(neighbour -> point(
+            coordinate.x + neighbour.x,
+            coordinate.y + neighbour.y))
+        .map(grid::get)
+        .filter(Objects::nonNull)
+        .reduce(Integer::sum)
+        .orElseThrow(IllegalArgumentException::new);
+  }
+
+  private Point getCoordinates(int position) {
     int x, y;
-    int root = calculateOddRoot(input);
+    int root = calculateOddRoot(position);
     int base = (root - 1) / 2 + 1;
 
     // Exactly the square value -> special case (base - 1)
-    if (root * root == input) {
+    if (root * root == position) {
       x = +base - 1;
       y = -base + 1;
     }
     // Vertical-right on the spiral
-    else if (input <= root * root + 1 * (root + 1)) {
+    else if (position <= root * root + 1 * (root + 1)) {
       x = +base;
-      y = -base + (input - (root * root + 0 * (root + 1)));
+      y = -base + (position - (root * root + 0 * (root + 1)));
     }
     // Horizontal-up on the spiral
-    else if (input <= root * root + 2 * (root + 1)) {
-      x = +base - (input - (root * root + 1 * (root + 1)));
+    else if (position <= root * root + 2 * (root + 1)) {
+      x = +base - (position - (root * root + 1 * (root + 1)));
       y = +base;
     }
     // Vertical-left on the spiral
-    else if (input <= root * root + 3 * (root + 1)) {
+    else if (position <= root * root + 3 * (root + 1)) {
       x = -base;
-      y = +base - (input - (root * root + 2 * (root + 1)));
+      y = +base - (position - (root * root + 2 * (root + 1)));
     }
     // Horizontal-down on the spiral
     else {
-      x = -base + (input - (root * root + 3 * (root + 1)));
+      x = -base + (position - (root * root + 3 * (root + 1)));
       y = -base;
     }
 
-    return abs(x) + abs(y);
+    return point(x, y);
   }
 
   private int calculateOddRoot(int input) {
@@ -63,8 +119,44 @@ public class Day03 implements DailyProblem<Integer> {
     return root;
   }
 
-  @Override
-  public int solveSecond(Integer input) {
-    throw new UnsupportedOperationException();
+  static class Point {
+
+    private final int x;
+    private final int y;
+
+    private Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    static Point point(int x, int y) {
+      return new Point(x, y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+
+      Point point = (Point) o;
+
+      return new EqualsBuilder()
+          .append(x, point.x)
+          .append(y, point.y)
+          .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+      return new HashCodeBuilder(17, 37)
+          .append(x)
+          .append(y)
+          .toHashCode();
+    }
   }
 }
--- src/test/java/be/inniger/advent/solutions/Day03Test.java
+++ src/test/java/be/inniger/advent/solutions/Day03Test.java
@@ -1,6 +1,7 @@
 package be.inniger.advent.solutions;
 
 import static be.inniger.advent.util.InputFileReader.read;
+import static java.lang.Integer.parseInt;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
@@ -28,7 +29,32 @@ public class Day03Test extends DailyTest {
 
   @Test
   public void validateFirstSolution() {
-    assertThat(problem.solveFirst(Integer.parseInt(read(FILE_NAME).get(0))))
+    assertThat(problem.solveFirst(parseInt(read(FILE_NAME).get(0))))
         .isEqualTo(438);
+  }
+
+  @Test
+  public void validateSecondSampleInputs() {
+    // These don't make sense, as the actual method calculates the next value surpassing the given input
+//    assertThat(problem.solveSecond(1))
+//        .isEqualTo(1);
+//
+//    assertThat(problem.solveSecond(2))
+//        .isEqualTo(1);
+//
+//    assertThat(problem.solveSecond(3))
+//        .isEqualTo(2);
+//
+//    assertThat(problem.solveSecond(4))
+//        .isEqualTo(4);
+//
+//    assertThat(problem.solveSecond(5))
+//        .isEqualTo(5);
+  }
+
+  @Test
+  public void validateSecondSolution() {
+    assertThat(problem.solveSecond(parseInt(read(FILE_NAME).get(0))))
+        .isEqualTo(266330);
   }
 }
