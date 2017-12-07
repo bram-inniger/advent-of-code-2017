d0945115e50dbdaea780a4792facc531110df6e5
Bram Inniger <braminniger@gmail.com>
2017-12-08 00:58:23 +0200

    Solved Day 7 - Part 2

--- src/main/java/be/inniger/advent/solutions/Day07.java
+++ src/main/java/be/inniger/advent/solutions/Day07.java
@@ -1,15 +1,21 @@
 package be.inniger.advent.solutions;
 
+import static java.util.Map.Entry;
 import static java.util.Objects.isNull;
 import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+// TODO - Fix the bug and clean up / simplify / shorten the code
 import be.inniger.advent.DailyProblem;
 
 public class Day07 implements DailyProblem<List<String>, String> {
@@ -18,19 +24,53 @@ public class Day07 implements DailyProblem<List<String>, String> {
 
   @Override
   public String solveFirst(List<String> input) {
-    return getRoot(assembleTree(input)).getName();
+    return assembleTree(input).getRoot().getName();
   }
 
   @Override
   public String solveSecond(List<String> input) {
-    throw new UnsupportedOperationException();
+    List<Program> unbalancedChildren = assembleTree(input).getPrograms().values()
+        .stream()
+        .filter(Program::hasUnequalChildren)
+        .map(Program::getChildren)
+        .limit(1) // FIXME - should not be needed, bug!
+        .flatMap(Collection::stream)
+//        .peek(System.out::println)
+        .collect(toList());
+
+    Map<Integer, Long> unbalancedChildrenWeights = unbalancedChildren
+        .stream()
+        .map(Program::getTotalWeight)
+        .collect(groupingBy(
+            identity(),
+            counting()));
+
+    int wrongAmount = unbalancedChildrenWeights.entrySet()
+        .stream()
+        .filter(entry -> entry.getValue() == 1)
+        .map(Entry::getKey)
+        .findAny()
+        .orElseThrow(IllegalArgumentException::new);
+
+    int rightAmount = unbalancedChildrenWeights.entrySet()
+        .stream()
+        .filter(entry -> entry.getValue() > 1)
+        .map(Entry::getKey)
+        .findAny()
+        .orElseThrow(IllegalArgumentException::new);
+
+    Program wrongChild = unbalancedChildren.stream()
+        .filter(program -> program.totalWeight == wrongAmount)
+        .findAny()
+        .orElseThrow(IllegalArgumentException::new);
+
+    return "" + (wrongChild.weight - wrongAmount + rightAmount);
   }
 
-  private Map<String, Program> assembleTree(List<String> input) {
+  private Tree assembleTree(List<String> input) {
     Map<String, Program> programs = input.stream()
         .map(NON_ALPHANUMERIC::split)
-        .map(line -> line[0])
-        .map(Program::new)
+        .map(line -> new Program(line[0], line[1]))
         .collect(toMap(
             Program::getName,
             identity()));
@@ -39,7 +79,11 @@ public class Day07 implements DailyProblem<List<String>, String> {
         .map(NON_ALPHANUMERIC::split)
         .forEach(line -> markParentChild(line, programs));
 
-    return programs;
+    Program root = getRoot(programs);
+
+    root.calculateTotalWeight();
+
+    return new Tree(root, programs);
   }
 
   private void markParentChild(String[] line, Map<String, Program> programs) {
@@ -62,19 +106,45 @@ public class Day07 implements DailyProblem<List<String>, String> {
         .orElseThrow(IllegalArgumentException::new);
   }
 
+  private class Tree {
+
+    private final Program root;
+    private final Map<String, Program> programs;
+
+    private Tree(Program root, Map<String, Program> programs) {
+      this.root = root;
+      this.programs = programs;
+    }
+
+    private Program getRoot() {
+      return root;
+    }
+
+    private Map<String, Program> getPrograms() {
+      return programs;
+    }
+  }
+
   private class Program {
 
     private final String name;
+    private final int weight;
     private final List<Program> children;
     private Program parent;
+    private int totalWeight;
 
-    private Program(String name) {
+    private Program(String name, String weight) {
       this.name = name;
+      this.weight = Integer.parseInt(weight);
       this.children = new ArrayList<>();
     }
 
     private String getName() {
       return name;
+    }
+
+    private List<Program> getChildren() {
+      return children;
     }
 
     private void setParent(Program parent) {
@@ -88,5 +158,39 @@ public class Day07 implements DailyProblem<List<String>, String> {
     private boolean isRoot() {
       return isNull(parent);
     }
+
+    private boolean hasUnequalChildren() {
+      return !children.isEmpty() &&
+          children.stream()
+              .noneMatch(Program::hasUnequalChildren) &&
+          children.stream()
+              .map(Program::getTotalWeight)
+              .distinct()
+              .count() > 1;
+    }
+
+    private int getTotalWeight() {
+      return totalWeight;
+    }
+
+    private int calculateTotalWeight() {
+      totalWeight = weight + children.stream()
+          .map(Program::calculateTotalWeight)
+          .reduce(Integer::sum)
+          .orElse(0);
+
+      return totalWeight;
+    }
+
+//    @Override
+//    public String toString() {
+//      return "Program{" +
+//          "name='" + name + '\'' +
+//          ", weight=" + weight +
+//          ", children=" + children.stream().map(Program::getName).collect(toList()) +
+//          ", parent=" + parent.name +
+//          ", totalWeight=" + totalWeight +
+//          '}';
+//    }
   }
 }
--- src/test/java/be/inniger/advent/solutions/Day07Test.java
+++ src/test/java/be/inniger/advent/solutions/Day07Test.java
@@ -41,14 +41,29 @@ public class Day07Test extends DailyTest {
   }
 
   @Override
-  @Ignore
   @Test
   public void validateSecondSampleInputs() {
+    assertThat(problem.solveSecond(asList(
+        "pbga (66)",
+        "xhth (57)",
+        "ebii (61)",
+        "havc (66)",
+        "ktlj (57)",
+        "fwft (72) -> ktlj, cntj, xhth",
+        "qoyq (66)",
+        "padx (45) -> pbga, havc, qoyq",
+        "tknk (41) -> ugml, padx, fwft",
+        "jptl (61)",
+        "ugml (68) -> gyxo, ebii, jptl",
+        "gyxo (61)",
+        "cntj (57)")))
+        .isEqualTo("60");
   }
 
   @Override
-  @Ignore
   @Test
   public void validateSecondSolution() {
+    assertThat(problem.solveSecond(read(FILE_NAME)))
+        .isEqualTo("646");
   }
 }
