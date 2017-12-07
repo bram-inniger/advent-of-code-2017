1a89fabee9945d2912492d16e5723ab1490dfaf9
Bram Inniger <braminniger@gmail.com>
2017-12-07 22:43:06 +0200

    Rewrote Day 7 to properly assemble the Tree first

--- src/main/java/be/inniger/advent/solutions/Day07.java
+++ src/main/java/be/inniger/advent/solutions/Day07.java
@@ -1,29 +1,24 @@
 package be.inniger.advent.solutions;
 
+import static java.util.Objects.isNull;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import be.inniger.advent.DailyProblem;
 
 public class Day07 implements DailyProblem<List<String>, String> {
 
+  private static final Pattern NON_ALPHANUMERIC = Pattern.compile("\\W+");
+
   @Override
   public String solveFirst(List<String> input) {
-    Set<String> nodes = new HashSet<>();
-    Set<String> childNodes = new HashSet<>();
-
-    input.stream()
-        .map(program -> Pattern.compile("\\W+").split(program))
-        .forEach(program -> markNodes(program, nodes, childNodes));
-
-    nodes.removeAll(childNodes);
-
-    return nodes.stream()
-        .findFirst()
-        .orElseThrow(IllegalArgumentException::new);
+    return getRoot(assembleTree(input)).getName();
   }
 
   @Override
@@ -31,13 +26,67 @@ public class Day07 implements DailyProblem<List<String>, String> {
     throw new UnsupportedOperationException();
   }
 
-  private void markNodes(String[] program, Set<String> nodes, Set<String> childNodes) {
-    nodes.add(program[0]);
+  private Map<String, Program> assembleTree(List<String> input) {
+    Map<String, Program> programs = input.stream()
+        .map(NON_ALPHANUMERIC::split)
+        .map(line -> line[0])
+        .map(Program::new)
+        .collect(toMap(
+            Program::getName,
+            identity()));
 
-    if (program.length > 2) {
-      childNodes.addAll(Arrays
-          .asList(program)
-          .subList(2, program.length));
+    input.stream()
+        .map(NON_ALPHANUMERIC::split)
+        .forEach(line -> markParentChild(line, programs));
+
+    return programs;
+  }
+
+  private void markParentChild(String[] line, Map<String, Program> programs) {
+    if (line.length > 2) {
+      Program parent = programs.get(line[0]);
+
+      Arrays.stream(line, 2, line.length)
+          .map(programs::get)
+          .forEach(child -> {
+            child.setParent(parent);
+            parent.addChild(child);
+          });
+    }
+  }
+
+  private Program getRoot(Map<String, Program> programs) {
+    return programs.values().stream()
+        .filter(Program::isRoot)
+        .findFirst()
+        .orElseThrow(IllegalArgumentException::new);
+  }
+
+  private class Program {
+
+    private final String name;
+    private final List<Program> children;
+    private Program parent;
+
+    private Program(String name) {
+      this.name = name;
+      this.children = new ArrayList<>();
+    }
+
+    private String getName() {
+      return name;
+    }
+
+    private void setParent(Program parent) {
+      this.parent = parent;
+    }
+
+    private void addChild(Program child) {
+      children.add(child);
+    }
+
+    private boolean isRoot() {
+      return isNull(parent);
     }
   }
 }
