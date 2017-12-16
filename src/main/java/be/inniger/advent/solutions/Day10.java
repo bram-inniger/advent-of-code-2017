224eeddadf20ad0d67c9739742f07c10d4fedf95
Bram Inniger <braminniger@gmail.com>
2017-12-16 17:40:08 +0200

    Solved Day 10 - Part 2

--- src/main/java/be/inniger/advent/solutions/Day10.java
+++ src/main/java/be/inniger/advent/solutions/Day10.java
@@ -1,46 +1,91 @@
 package be.inniger.advent.solutions;
 
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 import be.inniger.advent.DailyProblem;
 
-public class Day10 implements DailyProblem<List<Integer>, Integer> {
+public class Day10 implements DailyProblem<String, String> {
 
-  private int[] list;
+  private int listLength;
+
+  Day10() {
+    this(256);
+  }
 
   Day10(int listLength) {
-    list = IntStream.range(0, listLength)
-        .toArray();
+    this.listLength = listLength;
   }
 
   @Override
-  public Integer solveFirst(List<Integer> input) {
-    int pos = 0;
-    int skip = 0;
+  public String solveFirst(String input) {
+    List<Integer> lengths = Pattern.compile("\\W+")
+        .splitAsStream(input)
+        .map(Integer::parseInt)
+        .collect(toList());
 
-    for (int inputLength : input) {
-      reverse(pos, inputLength);
-      pos = (pos + inputLength + skip) % list.length;
-      skip++;
+    Hash hash = new Hash(lengths, listLength);
+    hash.performHashingRound();
+
+    return String.format("%d", hash.list.get(0) * hash.list.get(1));
+  }
+
+  @Override
+  public String solveSecond(String input) {
+    List<Integer> asciiCodes = input.chars()
+        .boxed()
+        .collect(toList());
+    asciiCodes.addAll(asList(17, 31, 73, 47, 23));
+
+    Hash sparseHash = new Hash(asciiCodes, listLength);
+    IntStream.range(0, 64).forEach(__ -> sparseHash.performHashingRound());
+
+    return IntStream.range(0, sparseHash.list.size() / 16)
+        .map(i -> IntStream.range(16 * i, 16 * i + 16)
+            .map(sparseHash.list::get)
+            .reduce(0, (a, b) -> a ^ b))
+        .mapToObj(value -> String.format("%02x", value))
+        .collect(joining());
+  }
+
+  private static class Hash {
+
+    private final List<Integer> lengths;
+    private final List<Integer> list;
+    private int currentPosition;
+    private int skipSize;
+
+    private Hash(List<Integer> lengths, int listLength) {
+      this.lengths = lengths;
+      this.list = IntStream.range(0, listLength)
+          .boxed()
+          .collect(toList());
+      this.currentPosition = 0;
+      this.skipSize = 0;
     }
 
-    return list[0] * list[1];
-  }
+    private void performHashingRound() {
+      for (int inputLength : lengths) {
+        reverse(inputLength);
+        currentPosition = (currentPosition + inputLength + skipSize) % list.size();
+        skipSize++;
+      }
+    }
 
-  @Override
-  public Integer solveSecond(List<Integer> input) {
-    throw new UnsupportedOperationException();
-  }
+    private void reverse(int length) {
+      for (int i = 0; i < length / 2; i++) {
+        int aPos = (currentPosition + i) % list.size();
+        int bPos = (currentPosition + length - i - 1) % list.size();
 
-  private void reverse(int pos, int length) {
-    for (int i = 0; i < length / 2; i++) {
-      int aPos = (pos + i) % list.length;
-      int bPas = (pos + length - i - 1) % list.length;
-
-      int tmp = list[aPos];
-      list[aPos] = list[bPas];
-      list[bPas] = tmp;
+        int tmp = list.get(aPos);
+        list.set(aPos, list.get(bPos));
+        list.set(bPos, tmp);
+      }
     }
   }
 }
--- src/test/java/be/inniger/advent/solutions/Day10Test.java
+++ src/test/java/be/inniger/advent/solutions/Day10Test.java
@@ -1,39 +1,50 @@
 package be.inniger.advent.solutions;
 
-import static be.inniger.advent.util.InputFileReader.readSameLineInts;
-import static java.util.Arrays.asList;
+import static be.inniger.advent.util.InputFileReader.readLine;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import be.inniger.advent.DailyTest;
 
 public class Day10Test extends DailyTest {
 
+  private final Day10 problem = new Day10();
+
   @Override
   @Test
   public void validateFirstSampleInputs() {
-    assertThat(new Day10(5).solveFirst(asList(3, 4, 1, 5)))
-        .isEqualTo(12);
+    assertThat(new Day10(5).solveFirst("3,4,1,5"))
+        .isEqualTo("12");
   }
 
   @Override
   @Test
   public void validateFirstSolution() {
-    assertThat(new Day10(256).solveFirst(readSameLineInts(FILE_NAME)))
-        .isEqualTo(11375);
+    assertThat(problem.solveFirst(readLine(FILE_NAME)))
+        .isEqualTo("11375");
   }
 
   @Override
-  @Ignore
   @Test
   public void validateSecondSampleInputs() {
+    assertThat(problem.solveSecond(""))
+        .isEqualTo("a2582a3a0e66e6e86e3812dcb672a272");
+
+    assertThat(problem.solveSecond("AoC 2017"))
+        .isEqualTo("33efeb34ea91902bb2f59c9920caa6cd");
+
+    assertThat(problem.solveSecond("1,2,3"))
+        .isEqualTo("3efbe78a8d82f29979031a4aa0b16a9d");
+
+    assertThat(problem.solveSecond("1,2,4"))
+        .isEqualTo("63960835bcdc130f0b66d7ff4f6a5a8e");
   }
 
   @Override
-  @Ignore
   @Test
   public void validateSecondSolution() {
+    assertThat(problem.solveSecond(readLine(FILE_NAME)))
+        .isEqualTo("e0387e2ad112b7c2ef344e44885fe4d8");
   }
 }
