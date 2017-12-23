40c20eec425fd9da081e3429606b02a489e7f597
Bram Inniger <braminniger@gmail.com>
2017-12-24 01:06:41 +0200

    Started Day 14 - Part 2, generate the grid instead of taking a shortcut only counting ones

--- src/main/java/be/inniger/advent/solutions/Day14.java
+++ src/main/java/be/inniger/advent/solutions/Day14.java
@@ -1,24 +1,38 @@
 package be.inniger.advent.solutions;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.IntStream;
+import static be.inniger.advent.util.Point.point;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import be.inniger.advent.DailyProblem;
 import be.inniger.advent.util.KnotHash;
+import be.inniger.advent.util.Point;
 
 public class Day14 implements DailyProblem<String, Integer> {
 
-  private static final Map<Integer, Integer> oneBits = getOneBits();
-
   @Override
   public Integer solveFirst(String input) {
-    return IntStream.range(0, 128)
-        .mapToObj(i -> input + "-" + i)
-        .map(KnotHash::hash)
-        .flatMapToInt(String::chars)
-        .map(oneBits::get)
-        .sum();
+    Set<Point> grid = new HashSet<>();
+
+    for (int i = 0; i < 128; i++) {
+      String knotHash = KnotHash.hash(input + "-" + i);
+
+      for (int j = 0; j < 32; j++) {
+        int value = Character.getNumericValue(knotHash.charAt(j));
+
+        // Check bit per bit of the single character (0 -> f) whether it is set or not
+        for (int k = 0; k < 4; k++) {
+          if ((value >> (3 - k) & 1) != 0) {
+            grid.add(point(i, 4 * j + k));
+          }
+        }
+      }
+    }
+
+    printGrid(grid, 8, 8);
+
+    return grid.size();
   }
 
   @Override
@@ -26,27 +40,15 @@ public class Day14 implements DailyProblem<String, Integer> {
     throw new UnsupportedOperationException();
   }
 
-  // TODO - Auto-create map using Streams
-  private static Map<Integer, Integer> getOneBits() {
-    Map<Integer, Integer> oneBits = new HashMap<>();
+  // TODO remove method
+  private void printGrid(Set<Point> grid, int xLim, int yLim) {
+    for (int i = 0; i < xLim; i++) {
 
-    oneBits.put((int) '0', 0); // 0000
-    oneBits.put((int) '1', 1); // 0001
-    oneBits.put((int) '2', 1); // 0010
-    oneBits.put((int) '3', 2); // 0011
-    oneBits.put((int) '4', 1); // 0100
-    oneBits.put((int) '5', 2); // 0101
-    oneBits.put((int) '6', 2); // 0110
-    oneBits.put((int) '7', 3); // 0111
-    oneBits.put((int) '8', 1); // 1000
-    oneBits.put((int) '9', 2); // 1001
-    oneBits.put((int) 'a', 2); // 1010
-    oneBits.put((int) 'b', 3); // 1011
-    oneBits.put((int) 'c', 2); // 1100
-    oneBits.put((int) 'd', 3); // 1101
-    oneBits.put((int) 'e', 3); // 1110
-    oneBits.put((int) 'f', 4); // 1111
+      for (int j = 0; j < yLim; j++) {
+        System.out.print(grid.contains(point(i, j)) ? '#' : '.');
+      }
 
-    return oneBits;
+      System.out.println();
+    }
   }
 }
